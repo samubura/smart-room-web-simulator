4082463365b8535a21aa7ce635adc62ea5af3a37
Samuele <samuele.bura98@gmail.com>
2021-12-02 19:25:42 +0100

    Fix tractor implementation

--- src/services/simulation/wrappers/smart-farming/SoilCheckerTractor.js
+++ src/services/simulation/wrappers/smart-farming/SoilCheckerTractor.js
@@ -12,6 +12,27 @@ class SoilCheckerTractor extends Tractor {
   constructor(id, env){
     super(id, 0, true, tractorIp, startingPosition, startingDirection)
   }
+
+  async mapAction(req, actionName, data) {
+    try {
+      return await super.mapAction(req, actionName, data)
+    } catch {
+      //continue switching
+      switch(actionName){
+        case 'checkSoilHumidity': {
+          return await this._checkSoilHumidity()
+        }
+        default: exceptions.propertyNotFound(this.id, propertyName)
+      }
+    }
+  }
+  
+  async _checkSoilHumidity(){
+    var url = `${this.ip}/properties/soilcondition`
+    var res = await axios.get(url)
+    console.log(res.data)
+    return {humidity: res.data.moisture};
+  }
 }
 
 module.exports.create = (id, env) => new SoilCheckerTractor(id, env)
